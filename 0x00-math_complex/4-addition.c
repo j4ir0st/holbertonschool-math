bda057af391a1a6f329dd112a51a55138469a029
j4ir0st <jairocast2@gmail.com>
2021-11-05 10:33:45 -0700

    adds two complex number

---
+++ 0x00-math_complex/4-addition.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+void addition(complex c1, complex c2, complex *c3)
+{
+	c3->re = c1.re + c2.re;
+	c3->im = c1.im + c2.im;
+}
