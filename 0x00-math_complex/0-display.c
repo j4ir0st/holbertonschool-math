237c167f64d7a14e7d777dc3d23e6ebfb55a62c3
j4ir0st <jairocast2@gmail.com>
2021-11-12 19:57:32 -0800

    prints a complex number

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -8,58 +8,28 @@
 
 void display_complex_number(complex c)
 {
-	double dre, dim;
+	char sgn;
 
-	if (c.im == 0)
+	if(c.im > 0)
 	{
-		printf("%.0f\n", c.re);
+		sgn = '+';
 	}
 	else
 	{
-		dre = fmod(c.re, 1);
-		dim = fmod(c.im, 1);
-		if (dre == 0)
-		{
-			printf("%.0f", c.re);
-		}
-		else
-		{
-			printf("%.1f", c.re);
-		}
-		if (c.im > 0)
-		{
-			printf(" + ");
-			if (c.im == 1)
-			{
-				printf("i\n");
-				return;
-			}
-			if (dim == 0)
-			{
-				printf("%.0fi\n", c.im);
-			}
-			else
-			{
-				printf("%.1fi\n", c.im);
-			}
-		}
-		if (c.im < 0)
-		{
-			printf(" - ");
-			if(c.im == -1)
-			{
-				printf("i\n");
-				return;
-			}
-			printf("%.0fi\n", -c.im);
-			if (dim == 0)
-			{
-				printf("%.0fi\n", c.im);
-			}
-			else
-			{
-				printf("%.1fi\n", c.im);
-			}
-		}
+		sgn = '-';
+		c.im = -c.im;
 	}
+	if (c.im == 0)
+	{
+		printf("%.9g\n", c.re);
+	}
+	else if (c.re == 0)
+	{
+		printf("%.9gi\n", c.im);
+	}
+	else
+	{
+		printf("%.9g %c %.9gi\n", c.re, sgn, c.im);
+	}
+
 }
