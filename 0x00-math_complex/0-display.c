a372184add6e818fc487952ee945ba943a5e9c13
j4ir0st <jairocast2@gmail.com>
2021-11-12 20:27:15 -0800

    prints a complex number negative includes

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -21,18 +21,11 @@ void display_complex_number(complex c)
 	}
 	if (c.im == 0)
 	{
-		printf("%.9g\n", c.re);
+		(c.re > 0) ? printf("%.9g\n", c.re) : printf("- %.9g\n", c.re);
 	}
 	else if (c.re == 0)
 	{
-		if (sgn == '+')
-		{
-			printf("%.9gi\n", c.im);
-		}
-		else
-		{
-			printf("%c %.9gi\n", sgn, c.im);
-		}
+		(sgn == '+') ? printf("%.9gi\n", c.im) : printf("%c %.9gi\n", sgn, c.im);
 	}
 	else
 	{
