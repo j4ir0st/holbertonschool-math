abcc59962f2f599a8612fb9063805e4fca20765f
j4ir0st <jairocast2@gmail.com>
2021-11-16 09:16:45 -0800

    adding header file holberton.h

--- 0x00-math_complex/holberton.h
+++ 0x00-math_complex/holberton.h
@@ -1,5 +1,6 @@
 #ifndef _HOLBERTON_H_
 #define _HOLBERTON_H_
+#include <math.h>
 
 /**
  * struct Complex - Short description
@@ -17,5 +18,13 @@ typedef struct Complex
 
 
 void display_complex_number(complex c);
+complex conjugate(complex c);
+double modulus(complex c);
+double argument(complex c);
+void addition(complex c1, complex c2, complex *c3);
+void substraction(complex c1, complex c2, complex *c3);
+void multiplication(complex c1, complex c2, complex *c3);
+void division(complex c1, complex c2, complex *c3);
+void complex_from_mod_arg(double m, double arg, complex *c3);
 
 #endif
